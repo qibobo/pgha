6886c61b2f57e0700d9998b90e056f743dac3a5c
qibobo <qiyangbj@cn.ibm.com>
2018-10-24 10:27:56 +0800

    path

--- startmaster.sh
+++ startmaster.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # postgres -D ../pgmaster
-
-DATA_DIR="./data"
-LOG_DIR="./log"
+BASE_PATH=$(cd "$(dirname "$0")"; pwd)
+DATA_DIR="${BASE_PATH}/data"
+LOG_DIR="${BASE_PATH}/log"
 LISTEN_HOST="127.0.0.1"
 PORT="5524"
 pg_ctl -o "-h ${LISTEN_HOST} -p ${PORT}" -w start -D "${DATA_DIR}" -l "${LOG_DIR}/startup.log"
